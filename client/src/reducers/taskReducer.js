474dda79de9e1aacb29e4651856c983aeae2bd33
he1snber8 <116442582+he1snber8@users.noreply.github.com>
2025-10-28 16:13:13 +0400

    Add taskReducer.js

---
+++ client/src/reducers/taskReducer.js
@@ -0,0 +1,1 @@
+// client/src/reducers/taskReducer.js\nconst initialState = [];\nexport default function taskReducer(state=initialState, action) { switch(action.type) { case "SET_TASKS": return action.payload; default: return state; } }
